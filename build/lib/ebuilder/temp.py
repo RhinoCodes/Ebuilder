8b8c127c47e187b623f0e23eeddabf153253c3b2
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-04-04 12:00:19 -0500

    Fix up PyPI junk, update the decorator example, and make ebuiler_decorators slightly faster

--- build/lib/ebuilder/temp.py
+++
@@ -1,8 +0,0 @@
-from ebuilder_core import *
-def index():
-    index = newpage('index')
-    header(index, "OOF")
-    index.commit()
-    print(index.buffer)
-
-index()
--- samples/hellogithub_with_decorators.py
+++ samples/hellogithub_with_decorators.py
@@ -1,17 +1,14 @@
-from ebuilder.ebuilder_core import *
-from inspect import getsource
+from ebuilder.ebuilder_decorators import *
 
-def page(func):
-    def wrapper():
-        file = func.__name__ # get name of the func
-        code = getsource(func) # get source code of the func
-        code = code.split("\n") # split the source code into a list by line
-        del code[0] # delete the decorator so it doesn't call this again and again and again...
-        code.insert(0, "from ebuilder.ebuilder_core import *") # Make sure it imports ebuilder core
-        code.insert(2, f"    {file} = newpage('{file}')") # Add a line that will create a new html page with ebuilder core 'newpage'
-        code.append(f"{file}()") # Append the function call so the code we edited will be called
-        code.insert(-1, "    {file}.final_commit()") # Add the final commit line so it will close the html file
-        code = '\n'.join(code) # Put the code back into a Python function format
-        exec(code) # execute the edited code
-        
-    return wrapper() # Run the function with all the edits
+@page
+def index():
+    title = header(index, "Hello Github!")
+    title.font("-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif")
+    title.center()
+    title.color("gray")
+    subtitle = header_two(index, "By RhinoCodes")
+    subtitle.font("-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif")
+    subtitle.center()
+    index.commit()
+
+# This and hellogithub_with_core.py both generate the same content except this one closes out the html file for you
