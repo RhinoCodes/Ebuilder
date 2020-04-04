f13e538c68c6a0b1c68c6fe09f289a1dadf2a37a
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-04-04 11:36:52 -0500

    Delete old junk from before renaming and sadly, add PyPI junk

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Ebuilder Ultra",
-    version="0.2.1",
+    name="Ebuilder",
+    version="0.2.2",
     author="Will F",
     author_email="forsbergw82@gmail.com",
     description="An upgraded version of good ol' Eebuilder, a static site generator that 'converts' Python 3 code to HTML 5.",
@@ -15,5 +15,5 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
-    url='https://github.com/RhinoCodes/Ebuilder-Ultra',
+    url='https://github.com/RhinoCodes/Ebuilder',
 )
